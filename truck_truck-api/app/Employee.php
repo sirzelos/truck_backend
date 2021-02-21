33a8a8713d18d1ad924ebe29a533becc4e4ed3d9
sirzelos <sikharin.k@ku.th>
2021-02-21 16:53:48 +0700

    login

--- truck_truck-api/app/Employee.php
+++ truck_truck-api/app/Employee.php
@@ -6,5 +6,6 @@ use Illuminate\Database\Eloquent\Model;
 
 class Employee extends Model
 {
-    //
+    public $timestamps =false;
+    protected $fillable=['name','email','salary'];
 }
--- truck_truck-api/routes/api.php
+++ truck_truck-api/routes/api.php
@@ -17,3 +17,13 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('employees','EmployeeController@getEmployee');
+
+Route::get('employee/{id}','EmployeeController@getEmployeeById');
+
+Route::post('addEmployee','EmployeeController@addEmployee');
+
+Route::put('updateEmployee/{id}','EmployeeController@updateEmployee');
+
+Route::delete('deleteEmployee/{id}', 'EmployeeController@deleteEmployee');
